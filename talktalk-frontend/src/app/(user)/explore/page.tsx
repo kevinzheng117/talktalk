24b40f1948b5582e1168eb616f2b064ffdda8ff6
kevinzheng117 <zhenghaoyang141@gmail.com>
2025-02-07 23:11:23 -0500

    added navbar and video feed components

---
+++ talktalk-frontend/src/app/(user)/explore/page.tsx
@@ -0,0 +1,9 @@
+import { VideoFeed } from "@/components/video-feed";
+
+export default function Explore() {
+  return (
+    <div className="flex justify-center items-center w-full h-full overflow-hidden">
+      <VideoFeed />
+    </div>
+  );
+}
---
+++ talktalk-frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,57 @@
+import { Telescope, UserRoundPen, Loader } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Explore",
+    url: "#",
+    icon: Telescope,
+  },
+  {
+    title: "Profile",
+    url: "#",
+    icon: UserRoundPen,
+  },
+  {
+    title: "Progress",
+    url: "#",
+    icon: Loader,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>TalkTalk</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
